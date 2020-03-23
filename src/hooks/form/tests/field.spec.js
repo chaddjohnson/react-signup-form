5b4143ed3f6874b0c46239f004a1603a7ddc8a88
Chad Johnson <chad.d.johnson@gmail.com>
2020-03-23 05:34:16 -0400

    Added useField hook test.

---
+++ src/hooks/form/tests/field.spec.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { useField } from '../';
+
+describe('useField hook', () => {
+  const TestField = ({ config }) => {
+    const field = useField(config);
+
+    return (
+      <>
+        <label>
+          Test field
+          <input
+            id="test-field"
+            value={field.value}
+            onChange={field.onChange}
+            onBlur={field.onBlur}
+          />
+        </label>
+        <div>{field.error}</div>
+      </>
+    );
+  };
+
+  // it('should validate on change', () => {
+  //   const wrapper = shallow(
+  //     <TestField
+  //       config={{
+  //         value: 'test value',
+  //         validates: [value => (value.length < 6 ? 'invalid' : '')]
+  //       }}
+  //     />
+  //   );
+
+  //   wrapper.find('input').prop('onChange')({
+  //     target: {
+  //       value: 'test'
+  //     }
+  //   });
+  //   expect(wrapper.find('div').text()).toBe('invalid');
+  // });
+
+  it('should validate on blur', () => {
+    const wrapper = shallow(
+      <TestField
+        config={{
+          value: 'test value',
+          validates: [value => 'invalid']
+        }}
+      />
+    );
+
+    wrapper.find('input').prop('onBlur')();
+    expect(wrapper.find('div').text()).toBe('invalid');
+  });
+});
